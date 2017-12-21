971a010c7297bff1458d9ff4ac5780d9dba9d422
Reto Stauffer <reto.stauffer@uibk.ac.at>
2017-12-21 21:13:16 +0100

    Added register/login to betform

--- user/views/bet.php
+++ user/views/bet.php
@@ -1,5 +1,6 @@
 <?php
 
+global $WTuser;
 
 // ------------------------------------------------------------------
 // Main part of the script. Calls update_bet_database if the user
@@ -7,16 +8,26 @@
 // Only allowed for logged-in users.
 // ------------------------------------------------------------------
 if ( ! is_user_logged_in() ) {
-   echo "<div class=\"wetterturnier-info error\">"
-       .__("You have to be registered and logged in to place your bet.","wpwt")
-       ."</div>";
+   printf("%s\n%s<br>\n%s %s %s %s %s. %s %s %s %s %s\n%s",
+       "<div class=\"wetterturnier-info error\">",
+       __("You have to be registered and logged in to place your bet.","wpwt"),
+       __("To register please","wpwt"),
+       "<a href=\"/wp-login.php?action=register\" target=\"_self\">",
+       __("register","wpwt"),
+       "</a>",
+       __("a new account","wpwt"),
+       __("If you already have an account just","wpwt"),
+       "<a href=\"wp-login.php\" target=\"_new\">",
+       __("login","wpwt"),
+       "</a>",
+       __("and enyoy the gambling!","wpwt"),
+       "</div>");
 
    // Show login form
    printf("<h1 class='entry-title'>%s</h1>",__("Login form","wpwt"));
    wp_login_form( );
 } else {
 
-   global $WTuser;
    global $WTbetclass;
 
    // - If not empty _POST, update database
